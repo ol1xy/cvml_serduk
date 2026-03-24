df9b524610dbd6023e3db685196156d551b07f59
ol1xy <aleksejserduk1@gmail.com>
2026-03-24 20:04:34 +0800

    Started learning

--- cyrrilic_mnist/train_model.py
+++ cyrrilic_mnist/train_model.py
@@ -46,3 +46,28 @@ augments = transforms.Compose([
     transforms.RandomRotation(15),
     transforms.ToTensor()
 ])
+
+class CyrillicCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(CyrillicCNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.relu = nn.ReLU()
+        
+        self.fc1 = nn.Linear(32 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x  = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+
+        x = x.view(-1, 32 * 7 * 7)
+
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
+
