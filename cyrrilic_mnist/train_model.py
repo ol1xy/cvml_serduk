9efe4cddfc83c0145b7ea25196cd3f47e4b764b5
ol1xy <aleksejserduk1@gmail.com>
2026-03-24 19:26:10 +0800

    Included requiredmodules

---
+++ cyrrilic_mnist/train_model.py
@@ -0,0 +1,10 @@
+from torch.utils.data import Dataset, DataLoader
+import os
+import torch
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.optim as optim
+
+class CyrrilicDataset(Dataset):
+    pass
