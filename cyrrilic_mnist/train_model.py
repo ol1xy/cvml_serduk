ab774dad00ab9e487b5c4d989f36cbfe5987eda0
ol1xy <aleksejserduk1@gmail.com>
2026-03-24 19:37:07 +0800

    Added a body of class

--- cyrrilic_mnist/train_model.py
+++ cyrrilic_mnist/train_model.py
@@ -5,6 +5,42 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.optim as optim
+import torchvision.transforms as transforms
 
 class CyrrilicDataset(Dataset):
-    pass
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+
+        self.image_paths = []
+        self.labels = []
+
+        self.classes = sorted(os.listdir(root_dir))
+        self.class_to_idx = {cls_name: i for i, cls_name in
+                             enumerate(self.classes)}
+        
+        for cls_name in self.classes:
+            cls_dir = os.path.join(root_dir, cls_name)
+            if os.path.isdir(cls_dir):
+                for img_name in os.listdir(cls_dir):
+                    self.image_paths.append(os.path.join(cls_dir,
+                                                        img_name))
+                    self.labels.append(self.class_to_idx[cls_name])
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        label = self.labels[idx]
+
+        image = Image.open(img_path).convert("L")
+
+        if self.transform:
+            image = ""
+
+        return image, label
+    
+train_transforms = transforms.Compose([
+    transforms.Resize((28, 28))
+])
