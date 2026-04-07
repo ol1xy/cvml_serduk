f8f4c9d0c10f786b98328b717c813605a7c18f44
ol1xy <aleksejserduk1@gmail.com>
2026-04-07 22:12:39 +0800

    Made demonstration

--- efnet_active/main.py
+++ efnet_active/main.py
@@ -9,126 +9,31 @@ import time
 from collections import deque
 import torch_directml
 from pathlib import Path
-
-device = torch_directml.device()
-save_path = Path(__file__).parent
-model_path = save_path / "model.pth"
-
-def build_model():
-    #TODO EFICCIENT_NET_B0 in HOMEWORK, size in entry parameters
-    weights = torchvision.models.AlexNet_Weights.IMAGENET1K_V1
-    model = torchvision.models.alexnet(weights=weights)
+import torchvision.models as models
+from train_model import predict
 
 
-    features = model.classifier[6].in_features
-    model.classifier[6] = nn.Linear(features, 1)
-    if model_path.exists():
-        model.load_state_dict(torch.load(model_path))
-#TODO WHY NO PARAMETERS??
-    for param in model.features.parameters():
-        param.requires_grad = False
-
-    return model.to(device=device)
-
-model = build_model()
-criterion = nn.BCEWithLogitsLoss()
-
-optimizier = torch.optim.Adam(
-    filter(lambda p: p.requires_grad,
-           model.parameters()),
-    lr = 0.0001
-)
-
-transform = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                         std = [0.229, 0.224, 0.225])
-])
-
-def train(buffer):
-    if len(buffer) < 10:
-        return None
-    model.train()
-    images,  labels = buffer.get_batch()
-    optimizier.zero_grad()
-    predictions = model(images).squeeze(1)
-    loss = criterion(predictions, labels)
-    loss.backward()
-    optimizier.step()
-    return loss.item()
-
-    
-
-def predict(frame):
-    model.eval()
-    tensor = transform(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    tensor = tensor.unsqueeze(0).to(device=device)
-    with torch.no_grad():
-        predicted = model(tensor).squeeze()
-        prob = torch.sigmoid(predicted).item()
-
-    label = "person" if prob > 0.5 else "no_person"
-    return label, prob
-
-class Buffer():
-    def __init__(self, maxsize = 16):
-        self.frames = deque(maxlen = maxsize)
-        self.labels = deque(maxlen = maxsize)
-
-    def append(self, tensor, label):
-        self.frames.append(tensor)
-        self.labels.append(label)
-
-    def __len__(self):
-        return len(self.frames)
-    
-    def get_batch(self):
-        images = torch.stack(list(self.frames)).to(device=device)
-        labels = torch.tensor(list(self.labels),
-                              dtype=torch.float32).to(device=device)
-        return images, labels
-
+model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.IMAGENET1K_V1)
+features = model.classifier[-1].in_features
+model.classifier[-1] = nn.Linear(features, 1)
+model.load_state_dict(torch.load('model.pth'))
 
 cap = cv2.VideoCapture(0)
 cv2.namedWindow("Camera", cv2.WINDOW_GUI_NORMAL)
-buffer = Buffer()
 count_labeled = 0
 
-# TODO graphics for efficient
 while True:
     _, frame = cap.read()
-    cv2.imshow("Camera", frame)
     key = cv2.waitKey(1) & 0xFf
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     if key == ord("q"):
         break
 
-    elif  key == ord("1"): #person
-        tensor = transform(image)
-        buffer.append(tensor, 1.0)
-        count_labeled += 1
-
-
-    elif key == ord("2"): #no person
-        tensor = transform(image)
-        buffer.append(tensor, 0.0)
-        count_labeled += 1
-
-    elif key == ord("p"): #predict
-        t = time.perf_counter()
-        label, confidence = predict(frame)
-        print(f"Elapsed time {time.perf_counter() - t}")
-        print(label, confidence)
-
-    elif key == ord("s"): #save_model
-        torch.save(model.state_dict(), model_path)
-
-    print(len(buffer), count_labeled)
-    if count_labeled >= buffer.frames.maxlen:
-        loss = train(buffer)
-        if loss:
-            print(f"Loss = {loss}")
-        count_labeled = 0
+    t = time.perf_counter()
+    label, confidence = predict(frame, model)
+    color = (0, 255, 0) if label == "person" else (0, 0, 255)
+    cv2.putText(frame, f"{label} ({confidence:.2f})", (50, 50), 
+        cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
+    
+    cv2.imshow("Camera", frame)
--- efnet_active/train_model.py
+++ efnet_active/train_model.py
@@ -11,61 +11,21 @@ import torch_directml
 from pathlib import Path
 import torchvision.models as models
 
-device = torch_directml.device()
 save_path = Path(__file__).parent
 model_path = save_path / "model.pth"
-
-def build_model():
-    #TODO EFICCIENT_NET_B0 in HOMEWORK, size in entry parameters
-    model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.IMAGENET1K_V1)
-
-    print(model.classifier)
-
-    features = model.classifier[-1].in_features
-    model.classifier[-1] = nn.Linear(features, 1)
-    if model_path.exists():
-        model.load_state_dict(torch.load(model_path))
-#TODO WHY NO PARAMETERS??
-    for param in model.features.parameters():
-        param.requires_grad = False
-
-    return model.to(device=device)
-
-model = build_model()
-criterion = nn.BCEWithLogitsLoss()
-
-optimizier = torch.optim.Adam(
-    filter(lambda p: p.requires_grad,
-           model.parameters()),
-    lr = 0.0001
-)
 
 transform = transforms.Compose([
     transforms.ToPILImage(),
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                         std = [0.229, 0.224, 0.225])
+                        std = [0.229, 0.224, 0.225])
 ])
 
-def train(buffer):
-    if len(buffer) < 10:
-        return None
-    model.train()
-    images,  labels = buffer.get_batch()
-    optimizier.zero_grad()
-    predictions = model(images).squeeze(1)
-    loss = criterion(predictions, labels)
-    loss.backward()
-    optimizier.step()
-    return loss.item()
-
-    
-
-def predict(frame):
+def predict(frame, model):
     model.eval()
     tensor = transform(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    tensor = tensor.unsqueeze(0).to(device=device)
+    tensor = tensor.unsqueeze(0)
     with torch.no_grad():
         predicted = model(tensor).squeeze()
         prob = torch.sigmoid(predicted).item()
@@ -73,63 +33,108 @@ def predict(frame):
     label = "person" if prob > 0.5 else "no_person"
     return label, prob
 
-class Buffer():
-    def __init__(self, maxsize = 16):
-        self.frames = deque(maxlen = maxsize)
-        self.labels = deque(maxlen = maxsize)
+if __name__ == "__main__":
 
-    def append(self, tensor, label):
-        self.frames.append(tensor)
-        self.labels.append(label)
+    def build_model():
+        #TODO EFICCIENT_NET_B0 in HOMEWORK, size in entry parameters
+        model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.IMAGENET1K_V1)
 
-    def __len__(self):
-        return len(self.frames)
-    
-    def get_batch(self):
-        images = torch.stack(list(self.frames)).to(device=device)
-        labels = torch.tensor(list(self.labels),
-                              dtype=torch.float32).to(device=device)
-        return images, labels
+        print(model.classifier)
 
+        features = model.classifier[-1].in_features
+        model.classifier[-1] = nn.Linear(features, 1)
+        if model_path.exists():
+            model.load_state_dict(torch.load(model_path))
+    #TODO WHY NO PARAMETERS??
+        for param in model.features.parameters():
+            param.requires_grad = False
 
-cap = cv2.VideoCapture(0)
-cv2.namedWindow("Camera", cv2.WINDOW_GUI_NORMAL)
-buffer = Buffer()
-count_labeled = 0
+        return model
 
-# TODO graphics for efficient
-while True:
-    _, frame = cap.read()
-    cv2.imshow("Camera", frame)
-    key = cv2.waitKey(1) & 0xFf
-    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    model = build_model()
+    criterion = nn.BCEWithLogitsLoss()
 
-    if key == ord("q"):
-        break
-
-    elif  key == ord("1"): #person
-        tensor = transform(image)
-        buffer.append(tensor, 1.0)
-        count_labeled += 1
+    optimizier = torch.optim.Adam(
+        filter(lambda p: p.requires_grad,
+            model.parameters()),
+        lr = 0.0001
+    )
 
 
-    elif key == ord("2"): #no person
-        tensor = transform(image)
-        buffer.append(tensor, 0.0)
-        count_labeled += 1
 
-    elif key == ord("p"): #predict
-        t = time.perf_counter()
-        label, confidence = predict(frame)
-        print(f"Elapsed time {time.perf_counter() - t}")
-        print(label, confidence)
+    def train(buffer):
+        if len(buffer) < 10:
+            return None
+        model.train()
+        images,  labels = buffer.get_batch()
+        optimizier.zero_grad()
+        predictions = model(images).squeeze(1)
+        loss = criterion(predictions, labels)
+        loss.backward()
+        optimizier.step()
+        return loss.item()
 
-    elif key == ord("s"): #save_model
-        torch.save(model.state_dict(), model_path)
+        
 
-    print(len(buffer), count_labeled)
-    if count_labeled >= buffer.frames.maxlen:
-        loss = train(buffer)
-        if loss:
-            print(f"Loss = {loss}")
-        count_labeled = 0
+
+
+    class Buffer():
+        def __init__(self, maxsize = 16):
+            self.frames = deque(maxlen = maxsize)
+            self.labels = deque(maxlen = maxsize)
+
+        def append(self, tensor, label):
+            self.frames.append(tensor)
+            self.labels.append(label)
+
+        def __len__(self):
+            return len(self.frames)
+        
+        def get_batch(self):
+            images = torch.stack(list(self.frames))
+            labels = torch.tensor(list(self.labels),
+                                dtype=torch.float32)
+            return images, labels
+
+
+    cap = cv2.VideoCapture(0)
+    cv2.namedWindow("Camera", cv2.WINDOW_GUI_NORMAL)
+    buffer = Buffer()
+    count_labeled = 0
+
+    # TODO graphics for efficient
+    while True:
+        _, frame = cap.read()
+        cv2.imshow("Camera", frame)
+        key = cv2.waitKey(1) & 0xFf
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        if key == ord("q"):
+            break
+
+        elif  key == ord("1"): #person
+            tensor = transform(image)
+            buffer.append(tensor, 1.0)
+            count_labeled += 1
+
+
+        elif key == ord("2"): #no person
+            tensor = transform(image)
+            buffer.append(tensor, 0.0)
+            count_labeled += 1
+
+        elif key == ord("p"): #predict
+            t = time.perf_counter()
+            label, confidence = predict(frame, model)
+            print(f"Elapsed time {time.perf_counter() - t}")
+            print(label, confidence)
+
+        elif key == ord("s"): #save_model
+            torch.save(model.state_dict(), model_path)
+
+        print(len(buffer), count_labeled)
+        if count_labeled >= buffer.frames.maxlen:
+            loss = train(buffer)
+            if loss:
+                print(f"Loss = {loss}")
+            count_labeled = 0
