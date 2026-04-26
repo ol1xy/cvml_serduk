69fbd0b7e178d65979ffe5b053b7ffb86a385e14
ol1xy <aleksejserduk1@gmail.com>
2026-04-26 13:17:11 +0800

    Cleaned from extra-trash and measured angle

--- pushups/main.py
+++ pushups/main.py
@@ -12,7 +12,7 @@ def get_angle(a, b, c):
     angle = angle + 360 if angle < 0 else angle
     return 360 - angle if angle > 180 else angle
 
-def detect_hands_up(annotator, keypoints):
+def detect_hands_up(annotated_obj, keypoints):
     nose_seen = (keypoints[0][0] > 0 and keypoints[0][1] > 0)
     eyes_seen = (keypoints[1][0] > 0 and keypoints[1][1] > 0 and keypoints[2][0] > 0 and keypoints[2][1] > 0)
     left_shoulder = keypoints[5]
@@ -23,6 +23,9 @@ def detect_hands_up(annotator, keypoints):
     right_wrist = keypoints[10]
 
     if nose_seen and eyes_seen:
+        # print(get_angle(left_shoulder,
+        #                 left_elbow,
+        #                 left_wrist))
         if (left_shoulder[1] > 
             left_elbow[1] > 
             left_wrist[1]) or (right_shoulder[1] > 
@@ -32,7 +35,7 @@ def detect_hands_up(annotator, keypoints):
             left_angle = get_angle(left_shoulder,
                                    left_elbow,
                                    left_wrist)
-            cv2.putText(annotated, f"Hands Up({left_angle:.1f})", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 1)
+            cv2.putText(annotated_obj, f"Hands Up({left_angle:.1f})", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 1)
             return True
     return None
 
@@ -50,7 +53,6 @@ while camera.isOpened():
 
     t = time.perf_counter()
     results = model(frame)
-    print(f"FPS {1 / (time.perf_counter() - t):.1f}")
     if not results:
         continue
 
@@ -58,17 +60,11 @@ while camera.isOpened():
     keypoints = result.keypoints.xy.tolist()
     if not keypoints:
         continue
-    print(keypoints)
 
     annotator = Annotator(frame)
     annotator.kpts(result.keypoints.data[0], result.orig_shape, 5, True)
     annotated = annotator.result()
     
-    if detect_hands_up(annotated, keypoints[0]):
-        if ps is None:
-            ps = playsound("MLG.mp3", block=False)
-        else:
-            if not ps.is_alive():
-                ps = None
+    detect_hands_up(annotated, keypoints[0])
 
     cv2.imshow("Pose", annotated)
